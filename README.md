012c3f73f69659e08d9be98c9a297749dac34095
Beyelon <118230672+Beyelon03@users.noreply.github.com>
2024-05-31 18:44:44 +0300

    configure

--- README.md
+++ README.md
@@ -15,6 +15,12 @@ This project is a website for selling books.
 
 1. Clone the repository: `git clone <your-repository-url>`
 2. Install dependencies: `npm install`
+3. Create new file '.env' and add this: 
+`PORT="YOUR_PORT"`
+`DB_URL="YOUR_DB_URL_CONNECT"`
+`JWT_SECRET="YOUR_JWT_SECRET_KEY"`
+
+
 
 ## Usage
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,46 @@
-import express, { Request, Response, NextFunction} from 'express';
-import appRoutes from "./routes/app.routes";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import cors from 'cors';
+import mongoose from 'mongoose';
+import fileUpload from 'express-fileupload';
+import express, { Request, Response, NextFunction } from 'express';
+import appRoutes from './routes/app.routes';
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 
+app.use(cors());
+app.use(fileUpload({}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use('/api', appRoutes);
 
-const PORT = 3000;
+const startServer = async () => {
+  try {
+    if (!DB_URL) {
+      throw new Error('DB_URL не определен.');
+    }
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+    await mongoose.connect(DB_URL);
+    console.log('Подключено к MongoDB.');
+
+    app.listen(PORT, () => {
+      console.log(`Сервер запущен: http://localhost:${PORT}.`);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('Ошибка:', error.message);
+    } else {
+      console.log('Произошла непредвиденная ошибка');
+    }
+    process.exit(1);
+  }
+};
+
+startServer();
